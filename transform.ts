d11038c2cddcb534f4da52c97be31eda025febf1
Gnlow <entrydark@gmail.com>
2020-12-18 23:51:16 +0900

    my first transformer

--- transform.ts
+++ transform.ts
@@ -2,6 +2,10 @@ import * as ts from 'typescript';
 
 const transformer = (program: ts.Program): ts.TransformerFactory<ts.SourceFile> => context => sourceFile => {
   const visitor = (node: ts.Node): ts.Node => {
+    if (ts.isVariableDeclaration(node)) {
+      return ts.updateVariableDeclaration(node, node.name, node.type, ts.createStringLiteral('world'));
+    }
+
     console.log(node.kind, `\t# ts.SyntaxKind.${ts.SyntaxKind[node.kind]}`);
     return ts.visitEachChild(node, visitor, context);
   };
